3bc90e2747a8b07aa7ded59a419eb968ff55774a
Mr.Trans <vitomychueng@163.com>
2018-12-03 23:36:59 +0800

    cannot write the bubble check tomorrow by trans

--- Algorithm/bubble_insertSort/bubbleInsertSort.cpp
+++ Algorithm/bubble_insertSort/bubbleInsertSort.cpp
@@ -15,21 +15,41 @@ template <typename T> class bubbleInsertSort{
 
         }
         T *arr;
-        void bubbleSort(T array[]);
-        void insertSort(T array[]);
+        void bubbleSort();
+        void insertSort();
+        int arr_size;
     private:
         bubbleInsertSort(const bubbleInsertSort&);
         bubbleInsertSort& operator=(const bubbleInsertSort&);
 };
 
-template <typename T> void bubbleInsertSort<T>::bubbleSort(T arr[]){
-    if(sizeof(arr)/sizeof(arr[0])){
-
+template <typename T> void bubbleInsertSort<T>::bubbleSort(){
+    if(arr_size <= 1){
+          return;
+    }
+    for(int i = 0; i < arr_size; i++){
+        bool flag = false;
+        for(int j = 0; j < arr_size-i-1;++j){
+            if(*(arr+j*sizeof(T)) > *(arr+(j+1)*sizeof(T))){
+                T tmp = *(arr+j*sizeof(T));
+                *(arr+j*sizeof(T)) = *(arr+(j+1)*sizeof(T));
+                *(arr+(j+1)*sizeof(T)) = tmp;
+                flag = true;
+            }
+        }
+        if(!flag)
+            break;     
     }
 }
 int main(){
-    int arr[10];
-    cout << sizeof(arr)/sizeof(arr[0]) << endl;
+    int arr[10] = {2,3,4,7,6,4,2,3,3,4};
+    bubbleInsertSort<int> bSort(arr);
+    bSort.arr_size = 10;
+    bSort.bubbleSort();
+    int i = 0;
+    while(i<10){
+        cout << arr[i] << endl;
+    }
     return 0;
 }
 
